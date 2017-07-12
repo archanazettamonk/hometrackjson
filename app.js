f9f556e3d2eb3887fd90b25caa8c06e88df929e5
Archana Rajput <archana@zettamonk.com>
2017-07-12 23:02:30 +1000

    fix port

--- app.js
+++ app.js
@@ -3,6 +3,6 @@
 var express = require('express');
 var app = express();
 var apiController = require('./controllers/apiController');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 apiController(app);
 app.listen(port);
